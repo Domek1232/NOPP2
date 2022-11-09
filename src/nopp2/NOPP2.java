7ccd5cae93f5f68bf08e5914db4540e9387ebfff
viktorkaa <83224403+viktorkaa@users.noreply.github.com>
2022-11-09 13:01:04 +0100

    5.feladat kész (p)

--- src/nopp2/NOPP2.java
+++ src/nopp2/NOPP2.java
@@ -19,25 +19,52 @@ public class NOPP2 {
         System.out.println("*   NOPPVDProject   *");
         System.out.println("*********************");
     }
+     public static boolean szokoevE(int x) {
+
+        if (((x % 4 == 0) && (x % 100 != 0)) || (x % 400 == 0)) {
+
+            return true;
+
+        }
+
+        return false;
+
+    }
     
  
     public static void main(String[] args) {
-        int year = 1998;
 
-    // Implementing our algorithm.
-    if (year % 400 == 0) {
-      System.out.println(year + " Ez egy szökőév.");
-    } 
-    else if (year % 100 == 0) {
-      System.out.println(year + " Ez nem egy szökőév..");
-    } 
-    else if (year % 4 == 0) {
-      System.out.println(year + " Ez egy szökőév..");
-    } 
-    else {
-      System.out.println(year + " Ez nem egy szökőév..");
+    // O készít egy metódust, ami eldönti hogy szökőév e
+    //if (year % 400 == 0) {
+    //  System.out.println(year + " Ez egy szökőév.");
+    //} 
+    //else if (year % 100 == 0) {
+    //  System.out.println(year + " Ez nem egy szökőév..");
+    //} 
+    //else if (year % 4 == 0) {
+    // System.out.println(year + " Ez egy szökőév..");
+    //} 
+    //else {
+    //  System.out.println(year + " Ez nem egy szökőév..");
+    //}
+    int num = 1234, forditott = 0;
+    
+    System.out.println("Eredeti szám: " + num);
+
+    // P készít egy metódust, 
+    //ami egy számként kapott paramétert kap, és visszaadja szövegként, fordított sorrendben.
+    while(num != 0) {
+    
+      int digit = num % 10;
+      forditott = forditott * 10 + digit;
+
+      num /= 10;
     }
+
+    System.out.println("Fordított: " + forditott);
   }
 }
+  
+
     
 
